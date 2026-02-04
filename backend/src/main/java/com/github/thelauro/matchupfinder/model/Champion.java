00ef8c3d662c89c663e024fc2056587159f49549
Lauro Gomes Martins Ouverney <laurogmouverney2@gmail.com>
2026-02-03 22:44:05 -0300

    refactor(lombok): add lombok to optimize model classes

--- backend/src/main/java/com/github/thelauro/matchupfinder/model/Champion.java
+++ backend/src/main/java/com/github/thelauro/matchupfinder/model/Champion.java
@@ -4,59 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class Champion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     private String name;
     private String imageUrl;
 
-    protected Champion(){
-    }
-
-    public Champion(String imageUrl, String name, Long id) {
-        this.imageUrl = imageUrl;
-        this.name = name;
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Champion champion)) return false;
-        return Objects.equals(id, champion.id) && Objects.equals(name, champion.name) && Objects.equals(imageUrl, champion.imageUrl);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, imageUrl);
-    }
 }
